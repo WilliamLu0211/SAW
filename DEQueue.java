962d80d65cd67fc8dacdf511e788122c848dac67
WilliamLu0211 <33496285+spectre0211@users.noreply.github.com>
2018-04-19 20:24:39 -0400

    so gay

--- DEQueue.java
+++ DEQueue.java
@@ -3,119 +3,204 @@
     L02 -- All Hands on Deque!
     2018-4-20 F */
 
+import java.util.NoSuchElementException;
+import java.util.Iterator;
+    
 public class DEQueue<T> implements Deque<T> {
 
     private DLLNode<T> _head, _tail;
     private int _size;
     
     public DEQueue() {
-	_head = _tail = null;
-	_size = 0;
+        _head = _tail = null;
+        _size = 0;
     }
 
     /* first */
     public void addFirst(E x) {
-	if ( isEmpty() ) {
-	    _head = _tail = new DLLNode(x, null, null);
-	} else {
-	    _head.setPrev( new DLLNode(x, null, _head) );
-	    _head = _head.getPrev();
-	}
-	_size ++;
-    }
-    
-    public boolean offerFirst(E x) {
-	
+        if ( isEmpty() ) {
+            _head = _tail = new DLLNode(x, null, null);
+        } else {
+            _head.setPrev( new DLLNode(x, null, _head) );
+            _head = _head.getPrev();
+        }
+        _size ++;
     }
 
     // throw exception if empty
     public E removeFirst() {
-	if ( isEmpty() )
-	    throw NoSuchElementException;
-	else {
-	    E rtnVal = _head.getCargo();
-	    if ( _size == 1 )
-		_head = _tail = null;
-	    else {
-		_head = _head.getNext();
-		_head.setPrev(null);
-	    }
-	    size--;
-	    return rtnVal;
-	}
+        if ( isEmpty() )
+            throw new NoSuchElementException();
+        E rtnVal = _head.getCargo();
+        _head = _head.getNext();
+        if ( _size == 1 )
+            _tail = _head;
+        else _head.setPrev( null );
+        size--;
+        return rtnVal;
     }
 
 
     // return null if empty
     public E pollFirst() {
-	if ( isEmpty() )
-	    return null;
-	else {
-	    removeFirst(); // modularity!!!
-	}
+        if ( isEmpty() )
+            return null;
+        return removeFirst(); // modularity!!!
     } 
 
     // throw exception if empty
     public E getFirst() {
-	if ( isEmpty() )
-	    throw NoSuchElementException;
-	else {
-	    return _head.getCargo(); 
-	}
+        if ( isEmpty() )
+            throw new NoSuchElementException();
+        return _head.getCargo(); 
     }
 
     // return null if empty
     public E peekFirst() {
-	if ( isEmpty() )
-	    throw NoSuchElementException;
-	else {
-	    getFirst();
-	}
-    } 
+        if ( isEmpty() )
+            return null;
+        return getFirst();
+    }
     
     public boolean removeFirstOccurrence(Object x) {
-	DLLNode tmp = _head;
-	while ( _tmp.getNext() ) {
-	    return _tmp.getCargo().equals(x);
-	}
-	return false;
+        DLLNode temp = _head;
+        while ( temp != null ) {
+            if ( temp.getCargo().equals(x) ) {
+                temp.getPrev().setNext( temp.getNext() );
+                temp.getNext().setPrev( temp.getPrev() );
+                _size --;
+                return true;
+            }
+            temp = temp.getNext();
+        }
+        return false;
     }
     /* first */
     
     /* last */
     public void addLast(E x) {
-	if ( isEmpty() ) {
-	    _tail = _head = new DLLNode(x, null, null);
-	} else {
-	    _tail.setNext( new DLLNode(_tail, null, x) );
-	    _tail = _tail.getNext();
-	}
-	_size ++;
+        if ( isEmpty() ) {
+            _tail = _head = new DLLNode(x, null, null);
+        } else {
+            _tail.setNext( new DLLNode(x, _tail, null) );
+            _tail = _tail.getNext();
+        }
+        _size ++;
     }
     
-    public boolean offerLast(E x) {
-	
+    // throw exception if empty
+    public E removeLast() {
+        if ( isEmpty() )
+            throw new NoSuchElementException();
+        E rtnVal = _tail.getCargo();
+        _tail = _tail.getPrev();
+        if ( _size == 1 )
+            _head = _tail;
+        else _tail.setNext( null );
+        size--;
+        return rtnVal;
     }
     
-    public E removeLast(); // throw exception if empty
+    // return null if empty
+    public E pollLast() {
+        if ( isEmpty() )
+            return null;
+        return removeLast();
+    }
     
-    public E pollLast(); // return null if empty
+    // throws exception if empty
+    public E getLast() {
+        if ( isEmpty() )
+            throw new NoSuchElementException();
+        return _tail.getCargo();
+    }
     
-    public E getLast(); // throw exception if empty
-    
-    public E peekLast(); // return null if empty
+    // return null if empty
+    public E peekLast() {
+        if ( isEmpty() )
+            return null;
+        return getLast();
+    }
     /* last */
     
     /* general */
-    public boolean isEmpty();
+    public boolean isEmpty() { return _size == 0; }
     
-    public boolean contains(Object x);
+    public boolean contains( Object x ) {
+        DLLNode temp = _head;
+        while ( temp != null ) {
+            if ( temp.getCargo().equals(x) )
+                return true;
+            temp = temp.getNext();
+        }
+        return false;
+    }
     
-    public Iterator<E> iterator();
+    private class MyIterator implements Iterator<E> {
+        
+        private DLLNode<E> _temp;
+        private boolean forward;
+        private boolean _okToRemove;
+        
+        public MyIterator( int d ) {
+            if ( d == 0 ) {
+                _temp = new DLLNode( null, null, _head );
+                forward = true;
+            } else {
+                _temp = new DLLNode( null, _tail, null );
+                forward = false;
+            }
+            _okToRemove = false;
+        }
+        
+        public boolean hasNext() {
+            if (forward) return _temp.getNext() != null;
+            else return _temp.getPrev() != null;
+        }
+        
+        public E next() {
+            if (forward) {
+                _temp = _temp.getNext();
+                if ( _temp == null )
+                    throw new NoSuchElementException();
+            } else {
+                _temp = _temp.getPrev();
+                if ( _temp == null )
+                    throw new NoSuchElementException();
+            }
+            _okToRemove = true;
+            return _temp.getCargo();
+        }
+        
+        public void remove() {
+            if ( ! _okToRemove )
+                throw new IllegalStateException("must call next() beforehand");
+            if ( _head == _tail )
+                _head = _tail = null;
+            else  if ( _temp = _head ) {
+                _head = _head.getNext();
+                _head.setPrev( null );
+            } else if ( _temp = _tail ) {
+                _tail = _tail.getPrev();
+                _tail.setNext( null );
+            } else {
+                _temp.getNext().setPrev( _temp.getPrev() );
+                _temp.getPrev().setNext( _temp.getNext() );
+            }
+            _okToRemove = false;
+            _size --;
+        }
+    }
     
-    public Iterator<E> 	descendingIterator();
+    public Iterator<E> iterator() {
+        return new MyIterator(0);
+    }
     
-    public int size();
+    public Iterator<E> 	descendingIterator() {
+        return new MyIterator(1);
+    }
+    
+    public int size() { return _size; }
     /* general */
     
 }
--- DLLNode.java
+++ DLLNode.java
@@ -1,82 +1,78 @@
 /*****************************************************
- * class DLLNode v1
+ * class DLLNode
  * Implements a node, for use in lists and other container classes.
- * Stores a String as cargo.
  *****************************************************/
 
-public class DLLNode
+public class DLLNode<T> {
+
+    private T _cargo;    //cargo may only be of type T
+    private DLLNode<T> _nextNode, _prevNode; //pointers to next, prev DLLNodes
+
+
+    // constructor -- initializes instance vars
+    public DLLNode( T value, DLLNode<T> prev, DLLNode<T> next ) 
 {
-  private String _cargo;    //cargo may only be of type String
-  private DLLNode _nextNode, _prevNode; //pointers to next, prev DLLNodes
+	_cargo = value;
+	_nextNode = next;
+	_prevNode = prev;
+    }
 
 
-  // constructor -- initializes instance vars
-  public DLLNode( String value, DLLNode prev, DLLNode next )
-  {
-    _cargo = value;
-    _nextNode = next;
-    _prevNode = prev;
-  }
+    //--------------v  ACCESSORS  v--------------
+    public T getCargo() { return _cargo; }
+
+    public DLLNode<T> getNext() { return _nextNode; }
+
+    public DLLNode<T> getPrev() { return _prevNode; }
+    //--------------^  ACCESSORS  ^--------------
 
 
-  //--------------v  ACCESSORS  v--------------
-  public String getCargo() { return _cargo; }
+    //--------------v  MUTATORS  v--------------
+    public T setCargo( T newCargo ) {
+	T foo = getCargo();
+	_cargo = newCargo;
+	return foo;
+    }
 
-  public DLLNode getNext() { return _nextNode; }
+    public DLLNode<T> setNext( DLLNode<T> newNext ) {
+	DLLNode<T> foo = getNext();
+	_nextNode = newNext;
+	return foo;
+    }
 
-  public DLLNode getPrev() { return _prevNode; }
-  //--------------^  ACCESSORS  ^--------------
-
-
-  //--------------v  MUTATORS  v--------------
-  public String setCargo( String newCargo )
-  {
-    String foo = getCargo();
-    _cargo = newCargo;
-    return foo;
-  }
-
-  public DLLNode setNext( DLLNode newNext )
-  {
-    DLLNode foo = getNext();
-    _nextNode = newNext;
-    return foo;
-  }
-
-  public DLLNode setPrev( DLLNode newPrev )
-  {
-    DLLNode foo = getPrev();
-    _prevNode = newPrev;
-    return foo;
-  }
-  //--------------^  MUTATORS  ^--------------
+    public DLLNode<T> setPrev( DLLNode<T> newPrev ) {
+	DLLNode<T> foo = getPrev();
+	_prevNode = newPrev;
+	return foo;
+    }
+    //--------------^  MUTATORS  ^--------------
 
 
-  // override inherited toString
-  public String toString() { return _cargo.toString(); }
+    // override inherited toString
+    public String toString() { return _cargo.toString(); }
 
 
-  //main method for testing
-  public static void main( String[] args )
-  {
-    //Below is an exercise in creating a linked list...
+    //main method for testing
+    public static void main( String[] args ) 
+{
+	//Below is an exercise in creating a linked list...
 
-    /*********************
-     //Create a node
-	DLLNode first = new DLLNode( "cat", null );
+	/*********************
+	//Create a node
+	DLLNode<String> first = new DLLNode<String>( "cat", null );
 
 	//Create a new node after the first
-	first.setNext( new DLLNode( "dog", null ) );
+	first.setNext( new DLLNode<String>( "dog", null ) );
 
 	//Create a third node after the second
-	first.getNext().setNext( new DLLNode( "cow", null ) );
+	first.getNext().setNext( new DLLNode<String>( "cow", null ) );
 
 	DLLNode temp = first; 
 	while( temp != null ) {
 	    System.out.println( temp );
 	    temp = temp.getNext();
 	}
-    ***********************/
-  }//end main
+	***********************/
+    }//end main
 
 }//end class DLLNode
